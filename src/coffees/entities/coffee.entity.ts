d3540033a26e0836e13f3eba7fc844760e4d5ecf
Yauri Attamimi <yauritux@gmail.com>
2022-09-10 02:10:39 +0700

    implement database

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,25 @@
+import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Flavor } from "./flavor.entity";
+
+@Entity('coffees')
 export class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
-  flavors: string[];
+
+  // @Column('json', { nullable: true })
+  @JoinTable()
+  @ManyToMany(
+    type => Flavor, 
+    (flavor) => flavor.coffees,
+    {
+      cascade: true,
+    }
+  )
+  flavors: Flavor[];
 }

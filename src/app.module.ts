56f99b7d92cd3e743c48755e1769e54e0df80a42
Yauri Attamimi <yauritux@gmail.com>
2022-09-09 16:24:40 +0700

    removes unnecessary imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoffeesController } from './coffees/coffees.controller';
-import { CoffeesService } from './coffees/coffees.service';
 import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
